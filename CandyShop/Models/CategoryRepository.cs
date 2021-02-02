76502c03cf0e91157ae7c2758a4befdbf5d6bf9a
Sunghyun Park <cmos00@nate.com>
2021-02-03 02:02:26 +0900

    초기 커밋.

---
+++ CandyShop/Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbcontext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbcontext = appDbContext;
+        }
+        public IEnumerable<Category> GetAllCategories => _appDbcontext.Categires;
+    }
+}
